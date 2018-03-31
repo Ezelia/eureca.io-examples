70407075671238e8c1cf34b50e23b438065a7006
Alaa-eddine <alaa.eddine@gmail.com>
2018-03-31 19:58:05 +0200

    Nex repo organization

---
+++ 007-webrtc/server.js
@@ -0,0 +1,66 @@
+﻿var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+var Eureca = require('../../');
+
+var eurecaServer = new Eureca.Server({transport:'webrtc', allow:['hello', 'sub']});
+
+eurecaServer.attach(server);
+
+
+
+//functions under "exports" namespace will
+//be exposed to client side
+eurecaServer.exports.hello = function () {
+    var client = eurecaServer.getClient(this.connection.id);
+    client.hello();
+    console.log('Hello from client');
+	return ('hello return');
+}
+eurecaServer.exports.hello.onCall = function(conn)
+{
+    console.log('Client called hello', conn.id);
+}
+
+
+eurecaServer.onConnect(function (conn) {
+    
+    console.log('new Client **');
+    var client = conn.clientProxy;
+    
+
+    
+    client.sub(10, 4).then(function (r) {
+        console.log('> 10 - 4 = ', r);
+    });
+    
+});
+
+eurecaServer.on('stateChange', (state) => {
+    console.log(' [info] State changed ', state);
+});
+
+
+server.on('request', function (request, response) {
+    var i;
+    
+    if (request.method === 'GET') {
+        
+        if (request.url.split('?')[0] === '/') {
+            var filename = __dirname + '/index.html';
+            fs.readFile(filename, function (err, data) {
+                var text = data.toString();
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.write(text);
+                response.end();
+            });
+        }
+    }
+
+});
+
+
+console.log('\033[96mlistening on localhost:8000 \033[39m');
+server.listen(8000);
---
+++ http-server/client.js
@@ -0,0 +1,9 @@
+﻿var Eureca = require('eureca.io');
+
+var client = new Eureca.Client({ uri: 'ws://localhost:8000/' });
+
+client.ready(function (proxy) {
+
+    proxy.hello();
+
+});
